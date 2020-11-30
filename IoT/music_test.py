603a5dba03ed9eeaa98cc9068ce1b7d86f0a301a
matt-pisini <pisini@usc.edu>
2020-11-30 15:55:00 -0800

    music

--- IoT/music_test.py
+++ IoT/music_test.py
@@ -12,49 +12,31 @@ RECORD_COMMAND = "arecord -D hw:1,0 -d {time} -f cd {file_name}.wav"
 VOLUME_UP = '='
 VOLUME_DOWN = '-'
 PAUSE = ' '
-#os.system("omxplayer mp3_test.mp3")
-#omxprocess = subprocess.Popen(['omxplayer', music_path],stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
 
-def spawnMusicChild(self):
-	child = pexpect.spawn('omxplayer ' + MUSIC_PATH.format(music = sample_music))
-	return child
+class MusicChild:
+	def __init__(self):
+		self.child = pexpect.spawn('omxplayer ' + MUSIC_PATH.format(music = sample_music))
 
-def spawnRecordingChild(self, record_time, file_name):
-	child = pexpect.spawn(RECORD_COMMAND.format(time = record_time, file = file_name))
-	return child
+	def changeMusicOutput(self, command):
+		self.child.send(command)
+		print(self.child.before)
 
-def changeMusicOutput(self, command, musicChild):
-	music_child.send(command)
-	print(music_child.before)
+	def terminateProcess(self):
+		self.child.close()
 
-def terminateProcess(self, child):
-	child.close()
-
+# def spawnRecordingChild(self, record_time, file_name):
+# 	child = pexpect.spawn(RECORD_COMMAND.format(time = record_time, file = file_name))
+# 	return child
 
 if __name__ == '__main__':
-	music_child = spawnMusicChild()
+	music_child = MusicChild()
 	# recording_child = spawnRecordingChild()
-	changeMusicOutput(VOLUME_DOWN, music_child)
+	music_child.changeMusicOutput(VOLUME_DOWN)
 	time.sleep(2)
-	changeMusicOutput(VOLUME_DOWN, music_child)
+	music_child.changeMusicOutput(VOLUME_DOWN)
 	time.sleep(2)
-	terminateProcess(music_child)
+	music_child.terminateProcess()
 
-# time.sleep(2)
-# child.send('-')
-# child.send('-')
-# child.send('-')
-# child.send('-')
-# time.sleep(2)
-# child.send(' ')
-# time.sleep(5)
-# child.send(' ')
-# child.send('=')
-# child.send('=')
-# child.send('=')
-# child.send('=')
-# time.sleep(2)
-#child.terminate()
 #omxprocess.call(input='-'.encode())
 #omxprocess.stdin.write('-'.encode())
 #time.sleep(1)
@@ -65,23 +47,3 @@ if __name__ == '__main__':
 #omxprocess.stdin.write('-'.encode())
 #output = omxprocess.communicate(input='-'.encode())[0]
 #output = omxprocess.communicate(input='='.encode())[0]
-
-#for i in range(3):
-	#print("Here")
-#	time.sleep(1)
-#	omxprocess.stdin.write('='.encode())
-#	try:
-		#output = omxprocess.communicate(input='='.encode())[0]
-		#omxplayer.check_call('='.encode())
-		#print("increased volume")
-#	except:
-		#print(output)
-
-#for i in range(3):
-#	time.sleep(1)
-	#omxprocess.stdin.write('-'.encode())
-#	output = omxprocess.communicate(input='-'.encode())[0]
-#	priint("decreased volume")
-
-#omxprocess.communicate(input='q'.encode())[0]
-#omxprocess.kill()
