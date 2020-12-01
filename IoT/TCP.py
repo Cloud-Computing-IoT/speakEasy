32d6e716717cebe22530570eb3983023e57f6a7b
matt-pisini <pisini@usc.edu>
2020-11-30 18:38:35 -0800

    TCP test

--- IoT/TCP.py
+++ IoT/TCP.py
@@ -22,7 +22,7 @@ class TCPsocket:
         l = f.read(1024)
         while(l):
             print("sending...")
-            self.sendData(l)
+            self.s.send(l)
             l = f.read(1024)
         f.close()
         print("done sending")
