7ec63134801f4b0d6c92ef2a3dec1c7c0778ea91
matt-pisini <pisini@usc.edu>
2020-12-03 20:40:57 -0800

    audio

--- IoT/audio.py
+++ IoT/audio.py
@@ -1,8 +1,11 @@
 import music_test as m 
 import time
 
+SONG1 = "mp3_test.mp3"
+SONG2 = "Lite_Weight.mp3"
+SONG3 = "24kGoldn.mp3"
 
-c = m.MusicChild()
+c = m.MusicChild(SONG2)
 r = m.RecordChild(20,"Lite_Weight_no_voice")
 
 time.sleep(25)
