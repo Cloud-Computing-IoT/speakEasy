48f5efb9644d0c846349df31ed6d6ffbcbed839f
matt-pisini <pisini@usc.edu>
2020-12-06 00:34:03 -0800

    threads

--- IoT/music_test.py
+++ IoT/music_test.py
@@ -35,6 +35,7 @@ RECORDING_LENGTH = 2
 REC_COUNT = 0
 ACCEL_DATA = '{  "linear_acceleration": {    "values": [      0.00235903263092041,      0.002854257822036743,      1.02996826171875E-4    ]  }}'
 COMMAND_QUEUE = Queue()
+RECORD_QUEUE = Queue()
 
 
 class MusicChild:
@@ -61,13 +62,18 @@ class RecordChild:
 		print("starting recording")
 		self.child = pexpect.spawn(RECORD_COMMAND.format(time = record_time, file_path = HOME_DIREC, file = file_name))
 		self.child.expect(pexpect.EOF)
-		global REC_COUNT
+		global REC_COUNT, RECORD_QUEUE
 		print("finished recording {}".format(REC_COUNT))
+		RECORD_QUEUE.put("{}{}.wav".format(HOME_DIREC,file_name))
 		REC_COUNT += 1
 		FINISHED_RECORDING = 1
 		# this process will automatically terminate after it records for 'record_time'
 		# maybe add automatically sending the file and deleting it?
-	
+
+class spawnThread:
+	def __init__(self, function, lock):
+		self.thread = threading.Thread(target=function, args=(lock,))
+		self.thread.start()
 
 #probably need to periodically clean up recordings or delete immediately after sending?
 def cleanUpRecordings(current_num):
@@ -81,41 +87,24 @@ def controlInterface(lock):
 	while True:
 		message = AWS_socket.receiveMessage()
 		AWS_socket.sendMessage("Received: " + message)
-		lock.acquire()
-		COMMAND_QUEUE.put(message)
-		lock.release()
 		if message.lower() == "stop":
 			AWS_socket.closeSocket()
 			break
-
-class spawnThread:
-	def __init__(self, function, lock):
-		self.thread = threading.Thread(target=function, args=(lock,))
-		self.thread.start()
-
-	def raise_exception(self): 
-		thread_id = self.get_id() 
-		res = ctypes.pythonapi.PyThreadState_SetAsyncExc(thread_id, 
-				ctypes.py_object(SystemExit)) 
-		if res > 1: 
-			ctypes.pythonapi.PyThreadState_SetAsyncExc(thread_id, 0) 
-			print('Exception raise failure')
+		elif message.lower() == "file":
+			AWS_socket.sendFile(RECORD_QUEUE.get())
 		else:
-			print("closing thread " + str(thread_id))
-
-	def get_id(self): 
-        # returns id of the respective thread 
-		# if hasattr(self, '_thread_id'): 
-		# 	return self._thread_id 
-		for id, thread in threading._active.items(): 
-			if thread is self: 
-				return id
+			lock.acquire()
+			COMMAND_QUEUE.put(message)
+			lock.release()
+		
 
 def startMusic(song_num):
 	global SONG_NUM
 	music_child = MusicChild(SONG_LIST[SONG_NUM])
 	print("Starting music with song: {}".format(SONG_LIST[SONG_NUM]))
 	SONG_NUM += 1
+	if SONG_NUM > len(SONG_LIST):
+		SONG_NUM = 0
 	return music_child
 		
 
@@ -125,6 +114,7 @@ if __name__ == '__main__':
 
 	command_lock = threading.Lock()
 	command_thread = spawnThread(controlInterface, command_lock)
+	RECORD_QUEUE.put("/home/pi/dummy.txt")
 	# command_thread = threading.Thread(target=controlInterface, args=(command_lock,))
 	# command_thread.start()
 	# AWS_socket.sendMessage("Started music with song " + SONG2)
@@ -144,8 +134,7 @@ if __name__ == '__main__':
 	while True:
 		if not music_child.child.isalive():
 			print("Song is over... :(")
-			music_child = MusicChild(SONG2)
-			print("Started music with song " + SONG2)
+			music_child = startMusic(SONG_NUM)
 		
 		if not COMMAND_QUEUE.empty():
 			command_lock.acquire()
@@ -155,8 +144,8 @@ if __name__ == '__main__':
 				music_child.terminateProcess()
 				# command_thread.raise_exception()
 				command_thread.thread.join()
+				print("Shutting down execution...")
 				sys.exit(1)
-				break
 			elif message.lower() == "next":
 				music_child.terminateProcess()
 				music_child = startMusic(SONG_NUM)
