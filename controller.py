3275457f48d661ebe7101447dd907b4b979170fe
matt-pisini <pisini@usc.edu>
2020-12-07 19:57:55 -0800

    controller

--- controller.py
+++ controller.py
@@ -2,8 +2,40 @@
 
 from audio_analyzer import analyze_audio
 
-if __name__ == "__main__":
+def parseObject(message, addr):
+    new_message = json.loads(message)
+    nums = {}
+    nums[str(addr[0])] = new_message["linear_acceleration"]["values"] #list of 3 accel values 
+    SERVER_MESSAGE_Q.put(nums)
 
+def processConnection(socket, addr):
+    try:
+        data = socket.recv(BUFFER_SIZE)
+    except:
+        print("Error: message not received")
+    parseObject(data, addr)
+    # SERVER_MESSAGE_Q.put(data.decode())
+
+def TCPserver(TCP_IP, TCP_PORT):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+        s.bind((TCP_IP, TCP_PORT))
+        s.listen(NUM_CONN)
+    except OSError as msg:
+        print(msg)
+        s.close()
+        sys.exit(1)
+    while True:
+        conn, addr = s.accept()
+        child = threading.Thread(target=processConnection, args=(conn,addr,))
+        # child = Process(target=processConnection, args=(conn,))
+        child.start()
+        print("Received connection from: " + str(addr[0]))
+    s.close()
+
+if __name__ == "__main__":
+    server_thread = threading.Thread(target=TCPserver, args=(socket.gethostname(),DEVICES_LISTEN_PORT,))
+    server_thread.start()
     volume = 5
     while(True):
         # Retrieve data from mic and accelerometers
