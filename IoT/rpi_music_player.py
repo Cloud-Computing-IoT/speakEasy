e2d6b48644a417d1d0691195ade17a90a1fd79e8
matt-pisini <pisini@usc.edu>
2020-12-08 23:16:12 -0800

    server script

--- IoT/rpi_music_player.py
+++ IoT/rpi_music_player.py
@@ -73,7 +73,7 @@ class MusicChild:
 		self.child.close()
 
 class RecordChild:
-	def __init__(self, lock, record_time, file_name):
+	def __init__(self, record_time, file_name):
 		global FINISHED_RECORDING
 		# FINISHED_RECORDING = 0
 		print("starting recording")
@@ -81,39 +81,31 @@ class RecordChild:
 		self.child.expect(pexpect.EOF)
 		global REC_COUNT, RECORD_QUEUE
 		print("finished recording {}".format(REC_COUNT))
-		# lock.acquire()
 		RECORD_QUEUE.put("{}{}.wav".format(HOME_DIREC,file_name))
-		# lock.release()
 		REC_COUNT += 1
 		if REC_COUNT >= FILE_LIMIT:
 			cleanUpRecordings(REC_COUNT-FILE_LIMIT)
-			#add how to delete the names off the RECORD_QUEUE
 		FINISHED_RECORDING = 1
 		# this process will automatically terminate after it records for 'record_time'
 		# maybe add automatically sending the file and deleting it?
 
-#probably need to periodically clean up recordings or delete immediately after sending?
+#keeps the last FILE_LIMIT recordings in home directory
 def cleanUpRecordings(current_num):
 	for file in os.listdir(HOME_DIREC):
-		# if ("rec" in file ) and (str(current_num) not in file):
 		if "rec{}".format(current_num) in file:
 			os.remove(os.path.join(HOME_DIREC, file))
 
-def controlInterface(command_lock, record_lock):
+def controlInterface():
 	try:
 		AWS_socket = tcp.TCPsocket()
 		AWS_socket.connect(AWS_IP, AWS_COMMAND_PORT)
 		while True:
 			message = AWS_socket.receiveMessage()
 			if message.lower() == "file":
-				# record_lock.acquire()
 				AWS_socket.sendFile(RECORD_QUEUE.get())
-				# record_lock.release()
 			elif message in COMMANDS:
 				# AWS_socket.sendMessage("Received: " + message)
-				# command_lock.acquire()
 				COMMAND_QUEUE.put(message)
-				# command_lock.release()
 				if message.lower() == "stop":
 					AWS_socket.closeSocket()
 					break
@@ -140,40 +132,21 @@ def sendRecording(file_path):
 if __name__ == '__main__':
 	music_child = startMusic(SONG_NUM)
 
-	command_lock = threading.Lock()
-	record_lock = threading.Lock()
-	# command_thread = spawnThread(controlInterface, [command_lock, record_lock])
-	command_thread = threading.Thread(target=controlInterface, args=(command_lock, record_lock))
+	# command_lock = threading.Lock()
+	# record_lock = threading.Lock()
+	command_thread = threading.Thread(target=controlInterface, args=())
 	command_thread.start()
-	# RECORD_QUEUE.put("/home/pi/dummy.txt")
-	# command_thread = threading.Thread(target=controlInterface, args=(command_lock,))
-	# command_thread.start()
-	# AWS_socket.sendMessage("Started music with song " + SONG2)
-	# controller = threading.Thread(target=musicControlInterface())
-	# controller.start()
-	# while True:
-	# 	if REC_COUNT >= 5:
-	# 		sys.exit(1)
-	# 	#creates recording
-	# 	if FINISHED_RECORDING == 1:
-	# 		print("rec: {}, state: {}".format(REC_COUNT,FINISHED_RECORDING))
-			# if REC_COUNT >= FILE_LIMIT:
-			# 		cleanUpRecordings(REC_COUNT)
-	# 		#create thread which will handle the recording subprocess
-	# 		x = threading.Thread(target=RecordChild, args=(RECORDING_LENGTH,"rec{}".format(REC_COUNT)))
-	# 		x.start()
+
+
 	while True:
 		if not music_child.child.isalive():
 			print("Song is over... :(")
 			music_child = startMusic(SONG_NUM)
 		
 		if not COMMAND_QUEUE.empty():
-			# command_lock.acquire()
 			message = COMMAND_QUEUE.get()
-			# command_lock.release()
 			if message.lower() == "stop":
 				music_child.terminateProcess()
-				# command_thread.raise_exception()
 				command_thread.join()
 				print("Shutting down execution...")
 				sys.exit(1)
@@ -183,21 +156,14 @@ if __name__ == '__main__':
 			elif message == "=" or "-" or " ":
 				output = music_child.changeMusicOutput(message)
 				print(output)
-			#maybe do this automatically
-			# elif message.lower() == "file":
-			# 	AWS_socket.sendFile(HOME_DIREC + "rec{}".format(rec_count))
-			# else:
-			# 	AWS_socket.sendMessage("Not a command. Try again.")
+
 		if FINISHED_RECORDING == 1:
-			# if REC_COUNT >= FILE_LIMIT:
-			# 		cleanUpRecordings(REC_COUNT)
 			#create thread which will handle the recording subprocess
 			FINISHED_RECORDING = 0
-			# x = spawnThread(RecordChild,lock=[record_lock],params=[RECORDING_LENGTH,"rec{}".format(REC_COUNT)] )
-			x = threading.Thread(target=RecordChild, args=(record_lock, RECORDING_LENGTH,"rec{}".format(REC_COUNT)))
+			x = threading.Thread(target=RecordChild, args=(RECORDING_LENGTH,"rec{}".format(REC_COUNT)))
 			x.start()
 
 		if not RECORD_QUEUE.empty():
+			#create thread which will send recording
 			x = threading.Thread(target=sendRecording, args=(RECORD_QUEUE.get(),))
 			x.start()
-			# sendRecording(RECORD_QUEUE.get())
---
+++ file_server_script.py
@@ -0,0 +1,46 @@
+# Controller script for speaker
+
+import sys
+import socket
+import threading
+
+NUM_CONN = 5
+BUFFER_SIZE = 1024
+
+
+def processFile(socket, addr, filePath):
+    try:
+        f = open(filePath, 'wb')
+    except:
+        print("error opening file for writing: " + filePath)
+    data = socket.recv(BUFFER_SIZE)
+    while(data):
+        f.write(data)
+        data = socket.recv(BUFFER_SIZE)
+    f.close()
+
+
+def TCPserver(TCP_IP, TCP_PORT):
+    global FILE_NUM
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+        s.bind((TCP_IP, TCP_PORT))
+        s.listen(NUM_CONN)
+    except OSError as msg:
+        print(msg)
+        s.close()
+        sys.exit(1)
+    while True:
+        conn, addr = s.accept()
+        child = threading.Thread(target=processFile, args=(conn,addr,"{}{}.wav".format(FILE_PATH,FILE_NUM),)) 
+        FILE_NUM += 1
+        child.start()
+        print("Received connection from: " + str(addr[0]))
+    s.close()
+
+if __name__ == "__main__":
+    inputs = sys.argv
+    LISTENING_PORT = inputs[1]
+    FILE_PATH = inputs[2]
+    FILE_NUM = 0
+    TCPserver("0.0.0.0",LISTENING_PORT)
